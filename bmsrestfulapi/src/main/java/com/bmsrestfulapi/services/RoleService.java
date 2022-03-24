6fb6947939535326f6498a4fc2e354ff684e8417
raginibhawsar58 <ragini8500@gmail.com>
2022-03-24 14:24:17 +0530

    assignrole added

--- bmsrestfulapi/src/main/java/com/bmsrestfulapi/services/RoleService.java
+++ bmsrestfulapi/src/main/java/com/bmsrestfulapi/services/RoleService.java
@@ -1,5 +1,8 @@
 package com.bmsrestfulapi.services;
 
+import com.bmsrestfulapi.exceptions.InvalidCredentialsException;
+
 public interface RoleService {
 
+	public String assignRole (Integer userId, String value) throws InvalidCredentialsException;
 }
